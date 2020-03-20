9f0e0d98143daf616b57899f93a4a0908f08b375
zzc <1439418936@qq.com>
2020-03-21 01:39:12 +0800

    图片统一了一下，能跳转了

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,42 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import home from '../views/task_management/home.vue';
+import login from '../views/user_management/login';
+import register from '../views/user_management/register';
+import registeP from '../views/vendor_management/registeP.vue';
+import mail_main from "../views/mail_management/mail_main";
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/login'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: '登录',
+    component: login
+  },
+  {
+    path: '/register',
+    name: '注册',
+    component: register
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: home,
+  },
+  {
+    path: '/registeP',
+    name: 'registeP',
+    component: registeP
+  },
+  {
+    path: '/mail',
+    name: 'mail_main',
+    component: mail_main
   }
 ]
 
